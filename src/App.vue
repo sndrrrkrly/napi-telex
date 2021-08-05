036a7ca19e80b583b1af8c7d3a0f22b2c4ea3825
Sandor Kiraly <business.tenshi@gmail.com>
2021-08-05 11:40:59 +0200

    responsive

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,15 @@
   <div id = "app">
     <div class = "min-w-screen min-h-screen bg-gray-800 flex items-center justify-center px-5 py-5">
       <div class = "w-full mx-auto rounded-xl bg-gray-100 shadow-lg p-10 text-gray-800 relative overflow-hidden min-w-80 max-w-3xl md:min-w-50 md:max-w-xl">
-        <p class = "text-gray-900 text-xl">
-          <em>"</em>
-        </p>
-
-        <h1 class = "text-gray-800 font-bold text-xl md:text-lg sm:text-sm text-center">
+        <h1 class = "leading-tight text-black font-bold text-xl md:text-lg sm:text-sm text-center">
           <div class="pre-formatted">
             {{ selectedComment }}
           </div>
         </h1>
 
-        <p class = "text-gray-900 text-xl text-right">
-          <em>"</em>
-        </p>
-
         <br />
 
-        <p class = "text-gray-900 font-light text-center">
+        <p class = "uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center">
           egy Telex kommentelő
         </p>
       </div>
@@ -34,7 +26,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
             </svg>
 
-            <a href = 'https://facebook.com/sandorkiraly0' class = 'text-gray-500 hover:text-gray-400'>
+            <a href = 'https://facebook.com/sandorkiraly0' class = 'text-gray-500 hover:text-gray-400 hover:underline'>
                 Facebook
             </a>
           </p>
@@ -46,12 +38,12 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
             </svg>
 
-            <h1 class="font-semibold text-gray-100 sm:text-lg"> 
+            <h1 class="font-semibold text-white sm:text-lg"> 
               napitelex
             </h1>
           </div>
           
-          <p class="text-gray-500">
+          <p class="text-gray-500 leading-tight">
             Telex legvállalhatatlanabb facebook <br /> kommentjeiből egy összeállítás naponta.
           </p>
         </div>
